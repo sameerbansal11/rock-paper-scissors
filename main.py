7b7d888008f78bc2d570a35271fc84ef3fb7be0e
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:29:32 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# rock-paper-scissors
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("Rock Paper Scissors game built with Python")
+    print("Project: rock-paper-scissors")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
